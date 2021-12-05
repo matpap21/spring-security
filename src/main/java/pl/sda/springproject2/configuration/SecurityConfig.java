23dec7bb3aee73b4519a711ceb205ef6389ce256
Mateusz <matpap21@wp.pl>
2021-12-05 18:31:13 +0100

    Spring Security dzialajacy +  autoryzacja na podstawie sesji bez .httpBasic()

--- src/main/java/pl/sda/springproject2/configuration/SecurityConfig.java
+++ src/main/java/pl/sda/springproject2/configuration/SecurityConfig.java
@@ -6,70 +6,76 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter { // ctrl + o -- > zobaczyc mozna metody jakie mozna wybrac
-    private final UserDetailsService userDetailsService;
-
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
-
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-        // reguly sa przetwarzane od gory do dolu i kolejnosc ,ma znaczenie
+        // ponizej autoryzacja na podstawie sesji bez .httpBasic()
+        // spring raz wyswietla nam formularz, nie korzysta z naglowka autorization tylko formularz przesyla nam dane do atoryzacji
+        // spring na serwerze tworzy nam sesje, zapamietuje uzytkownika i kazdy nastepny request ze strony formularza musi zawierac sesion id
         http
-               // .httpBasic()
-              //  .realmName("spring")
+               // .httpBasic() // jesli to uzyjemy to znikna nam formularze logowania, przegladarka owtorzy nam okienko z podaniem hasla i zutkownika
+                // zapamieta dane autoryzacyjne i przegldarka juz wysyla w kazdym requescie naglowek autorization
+
+              //  .realmName("spring") // nazwa krolestwa zabezpieczen
               //  .and()
-                .csrf()
+                .csrf() // zabezpieczenia
                 .disable()
                 .headers()
                 .and()
                 .authorizeRequests()
-               // .antMatchers("/todo/**", "/person/**").authenticated()
-                // USER ma dostep do /todo/list , ale nie ma dostepu do /todo/add
-                //ADMIN ma dostep do /todo/**
-                .antMatchers("/todo/list").hasAnyRole("USER","ADMIN")
-                .antMatchers("/todo/**", "/person/**").hasAnyAuthority("ROLE_ADMIN")
+                //.antMatchers("/todo/**", "/person/**").authenticated() // konfiguracja sciezek, wszystkie sciezki ktore maja wspolna polityke powinny byc dodwawane do tej metody
+                .antMatchers("/todo/list").hasAnyRole("USER", "ADMIN") //
+                .antMatchers("/todo/**", "/person/**").hasRole("ADMIN")
+                // ograniczamy
                 .antMatchers(HttpMethod.PUT,"/api/v2/todos/**").hasRole("USER")
                 .antMatchers(HttpMethod.DELETE,"/api/v2/todos/**").hasRole("USER")
                 .antMatchers(HttpMethod.PATCH,"/api/v2/todos/**").hasRole("USER")
                 .antMatchers(HttpMethod.POST,"/api/v2/todos/**").hasRole("USER")
-                .antMatchers(HttpMethod.GET,"/api/v2/todos").permitAll()
-                .anyRequest()
-                .permitAll()
+                .antMatchers(HttpMethod.GET,"/api/v2/todos/**").hasRole("USER")
+                .anyRequest().permitAll()
                 .and()
                 .formLogin()
+              //  .loginPage()
+              //  .defaultSuccessUrl("/")
                 .and()
                 .logout()
+              //  .clearAuthentication(true)
                 .logoutSuccessUrl("/")
                 .and();
 
+
     }
-    // zaznacz blok + ctrl + slash
+
 //    @Override
 //    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 //        auth
 //                .inMemoryAuthentication()
-//                .withUser("user").password("$2a$12$UKYmzmTSLqJ..Xotk3KrcOMx03bo28P1UMtgsGOzeS5gLxo6BvQIi").roles("USER","ADMIN").and()
-//                .withUser("ktos").password("ktos").roles("USER").and()
-//                .withUser("admin").password("admin").roles("ADMIN");
+//                .withUser("matpap21@wp.pl").password("spring").roles("USER","ADMIN")
+//                .and()
+//                .withUser("ktos").password("ktos").roles("USER")
+//                .and()
+//                .withUser("user").password("spring").roles("ADMIN");
 //    }
-
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(encoder());
+        auth
+                .inMemoryAuthentication()
+                .withUser("matpap21@wp.pl").password("$2a$12$9njK38D7aJZ.lrtXcVTna.QOs0t1f69HfP4tGqqK8leCjyLCtlKwK").roles("USER","ADMIN")
+                .and()
+                .withUser("ktos").password("$2a$12$3pgQeMFQpamAISHdX.jxWeoA5VHJ23hjuATUOKcdqk.r69nR7rv06").roles("USER")
+                .and()
+                .withUser("user").password("$2a$12$9njK38D7aJZ.lrtXcVTna.QOs0t1f69HfP4tGqqK8leCjyLCtlKwK").roles("ADMIN");
+    }
+    @Bean
+    public PasswordEncoder encoder(){
+       // return  NoOpPasswordEncoder.getInstance(); // hasla ustawione na sztywno
+        return new BCryptPasswordEncoder(); // hasla zakryptowne https://bcrypt-generator.com/
     }
 
-    @Bean // potrzebny by przekodowac password jako haslo a nie jako skrot
-    public PasswordEncoder encoder(){
-       // return NoOpPasswordEncoder.getInstance();
-        return new BCryptPasswordEncoder();
-    }
 }
